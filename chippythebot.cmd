c6ff71a0da8ee20f597a039297022cf7304756d5
Irza Pulungan <irzaip@gmail.com>
2018-08-30 17:48:41 +0700

    add more ignore

--- chippythebot.cmd
+++ chippythebot.cmd
@@ -10,7 +10,7 @@ SET PYTHONPATH=e:\data\python\3.6\Words\cipichatbot\;.
 cd e:\data\python\3.6\Words\cipichatbot\
 
 
-c:\Anaconda3\Scripts\pip install tensorflow keras tqdm sounddevice soundfile librosa Sastrawi
+c:\Anaconda3\Scripts\pip install tensorflow keras tqdm sounddevice soundfile librosa Sastrawi selenium gensim
 c:\anaconda3\python Cipi-theChatbot.py
 
 REM c:\anaconda3\python 
